dd38d39e612d6b7fcbc37820fa58354c12b652ec
SmrutiSikha08 <SikhaMahapatra2004@gmail.com>
2025-01-18 18:39:37 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,79 @@
+<section>
+  <footer class="footer-d">
+    <div class="container">
+      <div class="row">
+      <h1 style="color: white; margin-bottom: 20px;">Buy The Best Eyewear From Lenskart</h1>
+          <p>Lenskart Is The Leading E-Commerce Portal For Eyewear In India. It Has Revolutionised The Eyewear Industry In The Country With Its Omni-Channel Approach. From An Ever-Growing Number Of Offline Stores Across Major Cities In The Country To Innovative Integration Of Technology While Purchasing Online, Lenskart Caters To Every Customer With Several Deals And Offers.</p>
+          <br>
+          <p style="padding-bottom: 80px;">A One-Stop Online Solution For Purchasing Eyewear And Its Accessories, Lenskart Delivers Them Right At Your Doorstep With Convenient Methods Of Payment.
+            <a href="/sunglasses.html">Sunglasses</a> as well as <a href="/eyeglasses.html">Eyeglasses</a> Are Available For Men And Women In A Diverse Array Of Styles And Trendy Colours. If You Want To Try Out"<a href="/contact-lenses.html">Contact Lenses</a>, Pick The Ones Of Your Choice From The Extensive Variety Of Coloured Contact Lenses From Our Online Store.</p>
+        <div class="col-lg-3">
+        <div class="footer-col sp">
+        </div>
+        </div>
+        <div class="row">
+        <div class="col-lg-3">
+        <div class="footer-col">
+          <h3 style="font-size: 23px; color: #ffffff; margin-left: 28px; font-weight: 500;position: relative;">Services</h3>
+          <ul>
+            <li><a href="#">Store Locator</a></li>
+            <li><a href="#">Buying Guide</a></li>
+            <li><a href="#">Frame Size</a></li>
+          </ul>
+        </div>
+        </div>
+
+        <div class="col-lg-3" style="margin-left: -140px;">
+        <div class="footer-col">
+          <h3 style="font-size: 23px; color: #ffffff; margin-left: 28px; font-weight: 500;position: relative;">About Us</h3>
+          <ul>
+            <li><a href="#">We Are Hiring</a></li>
+            <li><a href="#">Refer And Earn</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Lenskart Coupons</a></li>
+          </ul>
+        </div>
+        </div>
+
+        <div class="col-lg-3" style="margin-left: -140px;">
+        <div class="footer-col">
+          <h3 style="font-size: 23px; color: #ffffff; margin-left: 28px; font-weight: 500;position: relative;">Help</h3>
+          <ul>
+            <li><a href="#">FAQ's</a></li>
+          </ul>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="ft" style="margin-left: 1140px; margin-top: -50px;">
+          <img src="https://static.lenskart.com/media/desktop/img/play-store.svg" alt="image">
+          <img src="https://static.lenskart.com/media/desktop/img/app-store.svg" alt="image">
+          <p class="ft2" style="color: #fff; font-size: 15px; font-weight: 400; margin-top: 20px; text-align: center; margin-right: 39px;">Download Lenskart App to buy 
+            <br>
+            Eyeglasses, Sunglasses and Contact Lenses
+        </p>
+        </div>
+  </footer>
+</section>
+
+<footer>
+  <div class="container">
+    <div class="col-lg-12 ivm">
+    <ul class="ft1" style="color: #fff; display: flex; margin-top: 30px;">
+            <li class="ft2" style="margin-right: 40px;">T & C</li>
+            <li class="ft2" style="margin-right: 40px;">Privacy</li>
+            <li class="ft2" style="margin-right: 40px;">Disclaimer</li>
+            <li class="ft2" style="margin-left: 700px; font-size: 15px; ">Version 1.0.0 ║ Follow Us 
+              <i class="bi bi-facebook" style="margin-left: 10px; font-size: 20px;"></i>
+              <i class="bi bi-instagram" style="margin-left: 10px; font-size: 20px;"></i>
+              <i class="bi bi-twitter" style="margin-left: 10px; font-size: 20px;"></i>
+            </li>
+          </ul>     
+    </div>
+  </div>
+</footer>
+<script src="js/lenskart.js"></script>
+</body>
+</html>
+ 
